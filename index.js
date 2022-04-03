d8dcc6710565872051a58345a45185dd556ff090
Fadel <fadel.g.heji@gmail.com>
2022-04-02 17:44:02 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+    
+
+    function superbowlWin(record){
+      
+        let isWinningYear = (record) => record.result ==="W"
+       
+        const winningYearObject = record.find(isWinningYear)
+        
+        if (winningYearObject){
+            return winningYearObject.year
+        }
+
+    }
